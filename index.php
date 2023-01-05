ba1afeb3c0891f5072118818c82ab013de8e8e57
Darlan <darlaneduardoguimaraes@gmail.com>
2023-01-05 17:00:17 -0300

    Nav complete

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wiking</title>
+    <link rel="stylesheet" href="global.css">
+    <link rel="stylesheet" href="fonts/Fonts/WEB/css/satoshi.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <div class="content">
+            <section class="left">
+                <img src="assets/logo_wiki.svg" alt="Logo da WIKING">
+            </section>
+            <section class="center">
+                    <ul>
+                        <li>BOAS VINDAS</li>
+                        <li>POSTS</li>
+                        <li>AUTORES</li>
+                        <li>AJUDA</li>
+                    </ul>
+            </section>
+            <section class="right">
+                <a href="#" ><button class="button_white">ENTRAR</button></a>
+                <a href="#"><button class="button_black">REGISTRAR-SE</button></a>
+            </section>
+        </div>
+    </header>
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
