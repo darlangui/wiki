c4c46bfe5c3ce74524d3ee9c19c67a343d2759b8
Darlan <darlaneduardoguimaraes@gmail.com>
2023-01-10 23:23:27 -0300

    posts struct main

--- pages/posts/index.php
+++ pages/posts/index.php
@@ -9,9 +9,87 @@
     <link rel="stylesheet" href="../../global.css">
     <link rel="stylesheet" href="../../style.css">
     <link rel="stylesheet" href="../../fonts/Fonts/WEB/css/satoshi.css">
+    <link rel="stylesheet" href="style.css">
     <link rel="icon" href="../../assets/logo_wk.svg">
 </head>
 <body>
+    <header>
+        <div class="content">
+            <section class="left">
+                <a href="index.php"><img src="assets/logo_wiki.svg" alt="Logo da WIKING"></a>
+            </section>
+            <section class="center">
+                <ul>
+                    <a href="../../"><li class="li_selected">BOAS VINDAS</li></a>
+                    <a href="#"><li class="li_selected">POSTS</li></a>
+                    <li class="li_selected">AUTORES</li>
+                    <li class="li_selected">AJUDA</li>
+                </ul>
+            </section>
+            <section class="right">
+                <a href="pages/login" ><button class="button_white">ENTRAR</button></a>
+                <a href="pages/register"><button class="button_black">REGISTRAR-SE</button></a>
+            </section>
+        </div>
+    </header>
+    <main>
+        <section class="writer_content">
+            <h1> POSTS </h1>
+            <h2> As mais diversas informações sobre o mundo.</h2>
+        </section>
+        <section class="cards_content">
+            <div class="left">
+                <div class="card">
+                    <h3>Resources</h3>
+                    <span>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ipsum lectus, ltrices venenatis.
+                    </span>
+                </div>
+            </div>
+            <div class="right">
+                <span> Ultimos Posts </span>
+                <div class="card">
+                    <img src="" alt="" srcset="">
+                    <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ipsum lectus, ltrices</h3>
+                </div>
+                <div class="card">
+                    <img src="" alt="" srcset="">
+                    <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ipsum lectus, ltrices</h3>
+                </div>
+                <div class="card">
+                    <img src="" alt="" srcset="">
+                    <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ipsum lectus, ltrices</h3>
+                </div>
+            </div>
+        </section>
+        <section class="main_content">
+            <h3>Todas as Publicações</h3>
 
+        </section>
+    </main>
+    <footer>
+        <section class="option_content">
+            <a href="index.php"><img src="assets/logo_wiki_white.svg" alt="Logo da WIKING"></a>
+            <ul>
+                <li><span>info</span></li>
+                <li><a href="#">Posts</a></li>
+                <li><a href="#">Autores</a></li>
+                <li><a href="#">Ajuda</a></li>
+            </ul>
+            <ul>
+                <li><span>social</span></li>
+                <li><a href="#">Twitter</a></li>
+                <li><a href="#">Instagram</a></li>
+            </ul>
+        </section>
+        <section class="last_linear_content">
+            <div class="linear"></div>
+            <div class="last">
+                <h3>2023</h3>
+                <h3>Tamplete by Darlan</h3>
+                <h3>2023</h3>
+            </div>
+        </section>
+    </footer>
 </body>
 </html>
